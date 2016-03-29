53b044750348e1ab5247da6110190432c2670d73
Jessica Oakes <oakes050@umn.edu>
2016-03-29 10:32:10 -0500

    Modified buttons

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+
+function printList(anObj) {
+  $("#display > ol").append("<li>" + "Name: " + anObj.firstname + " " + anObj.lastname +"</li>");
+
+  var lastli = $("#display > ol").children().last();
+
+  lastli.append("<span>" + "EmplID: " + anObj.emplID + "</span>");
+  lastli.append("<span>" + "Title: " + anObj.jobtitle + "</span>");
+  lastli.append("<span>" + "Review Score: " + anObj.reviewscore + "</span>");
+  lastli.append("<span>" + "Salary: " + anObj.jobsalary + "</span>");
+
+  var remove = $("<input type=\"button\" value=\"Remove\" class=\"removeButton\"/>");
+  lastli.append(remove);
+
+  lastli.css("padding-top", "10px");
+  $("span").css("padding-left", "10px");
+  remove.css("padding-left", "10px");
+  console.log(anObj);
+}
+
+
 $(function() {
+
+  $("#display").append("<ol></ol>");
 
   $("#myForm").on("submit", function(event) {
     event.preventDefault();
-  });
 
-  var emplDataArray = [];
-  $("#submit").on("click", function(event) {
+    var emplDataArray = [];
     var emplData = {};
     emplData.lastname = $("#lastname").val();
     emplData.firstname = $("#firstname").val();
@@ -14,7 +35,13 @@ $(function() {
     emplData.reviewscore = $("#reviewscore").val();
     emplData.jobsalary = $("#jobsalary").val();
 
+    printList(emplData);
     emplDataArray.push(emplData);
+  });
+
+  $("body").on("click", ".removeButton", function(event) {
+    // console.log(this);
+    $(this).parent().remove();
   });
 
 });
