2318fef148a16f6e592fefa196c5bf1f52e61581
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-03 09:01:16 -0500

    Adds CSS styling, fixes search functionality

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(function() {
-  console.log("Everything works!");
+  // console.log("Everything works!");
 
   $("#randomButton").on("click", function() {
     var giphyTag = "";
     /*  Assign tag input value to giphyTag, replacing any spaces if necessary (apparently some giphy tags have spaces)*/
     giphyTag = $("#tagInput").val().replace(/ /g, "+");
-    console.log(giphyTag);
 
     getRandomGiphy(giphyTag);
   }); //  Random/Tag Giphy Button
@@ -13,9 +12,8 @@ $(function() {
   $("#searchButton").on("click", function() {
     var giphySearch = "";
     giphySearch = $("#regInput").val().replace(/ /g, "+");
-    console.log(giphySearch);
 
-    getSearchGiphy(giphySearch.toString());
+    getSearchGiphy(giphySearch);
   }); //  Normal Search Button
 
   function getRandomGiphy(tagSearch) {
@@ -23,19 +21,24 @@ $(function() {
       {api_key: "dc6zaTOxFJmzC",
       tag: tagSearch}).done(function(response) {
       var randomPic = response;
-      // console.log(randomPic);
-      appendRandomPic(randomPic);
+
+      $("#display").empty();  //  Clear display
+      $("#display").append("<img src=" + response.data.image_url + ">"); //  Append pic to display
+
+
       }).fail(function(response) {
         console.log("fail");
     });
   } //  getRandomGiphy
 
   function getSearchGiphy(normalSearch) {
+    if (normalSearch === "") {
+      alert("Please input a search or click \"Random Giph\" to randomly generate one. Thank you :D");
+    }
     $.get("http://api.giphy.com/v1/gifs/search", {
       q: normalSearch,
       api_key: "dc6zaTOxFJmzC"}).done(function(response) {
         $("#display").empty();
-        console.log(response.data.length);
         for (var it = 0; it < response.data.length; it++) {
           $("#display").append("<img src=\"" + response.data[it].images.fixed_height.url + "\" />");
         } //  for loop
@@ -43,9 +46,4 @@ $(function() {
         console.log("search fail");
     });
   } //  getSearchGiphy
-
-  function appendRandomPic(picture) {
-    $("#display").empty();  //  Clear display
-    $("#display").append("<img src=" + picture.data.image_url + ">"); //  Append pic to display
-  } //  appendRandomPic
 }); //  Doc-ready function
