39d3765376e847f2f2d21acdbd38f013daaa8a84
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-03 10:02:45 -0500

    Adds enter key functionality & text field clear, more CSS styling

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ $(function() {
 
   $("#randomButton").on("click", function() {
     var giphyTag = "";
-    /*  Assign tag input value to giphyTag, replacing any spaces if necessary (apparently some giphy tags have spaces)*/
+    /*  Assign tag input value to giphyTag, replacing any spaces if necessary (apparently some giphy tags have spaces as seen on http://giphy.com/search/tags)*/
     giphyTag = $("#tagInput").val().replace(/ /g, "+");
-
     getRandomGiphy(giphyTag);
   }); //  Random/Tag Giphy Button
 
@@ -15,6 +14,36 @@ $(function() {
 
     getSearchGiphy(giphySearch);
   }); //  Normal Search Button
+
+  $("#tagInput").keypress(function(event) {
+    var giphyTag = "";
+    var code = event.which;
+    if (code == 13) {
+      giphyTag = $("#tagInput").val().replace(/ /g, "+");
+      console.log(giphyTag);
+      getRandomGiphy(giphyTag);
+    }
+  }); //  Random/Tag Search Enter Key event
+
+  $("#regInput").keypress(function(event) {
+    var giphySearch = "";
+    var code = event.which;
+    if (code == 13) {
+      giphySearch = $("#regInput").val().replace(/ /g, "+");
+      console.log(giphySearch);
+      getSearchGiphy(giphySearch);
+    }
+  }); //  Normal Search Enter Key event
+
+  $("#regInput").focus(
+    function(){
+      $(this).val("");
+  }); //  Clear field when clicked again, only works if input field is not selected
+
+  $("#tagInput").focus(
+    function(){
+      $(this).val("");
+  }); //  Clear field when clicked again, only works if input field is not selected
 
   /*  Random/Tag Giphy Search function
   **  Empty the display, append picture received from random API
@@ -28,7 +57,7 @@ $(function() {
       $("#display").append("<img src=" + response.data.image_url + ">"); //  Append pic to display
       }).fail(function(response) {
         console.log("fail");
-    });
+    }); //  AJAX
   } //  getRandomGiphy
 
   /*  Normal Giphy Search function
@@ -42,14 +71,14 @@ $(function() {
     $.get("http://api.giphy.com/v1/gifs/search", {
       q: normalSearch,
       api_key: "dc6zaTOxFJmzC"}).done(function(response) {
-        $("#display").empty();
+        $("#display").empty();  //  Clear display
         // console.log(response);
 
         for (var it = 0; it < response.data.length; it++) {
-          $("#display").append("<img src=\"" + response.data[it].images.fixed_height.url + "\" />");
-        } //  for loop
+          $("#display").append("<img src=\"" + response.data[it].images.original.url + "\" />");
+        } //  for loop, append all images to display
       }).fail(function(response){
         console.log("search fail");
-    });
+    }); //  AJAX
   } //  getSearchGiphy
 }); //  Doc-ready function
