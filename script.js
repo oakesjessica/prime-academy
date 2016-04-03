1b16e636efb47b1ddf4817148232290bf6f108c9
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-03 09:30:46 -0500

    Adds comments for functions

--- script.js
+++ script.js
@@ -16,21 +16,25 @@ $(function() {
     getSearchGiphy(giphySearch);
   }); //  Normal Search Button
 
+  /*  Random/Tag Giphy Search function
+  **  Empty the display, append picture received from random API
+  **  Console.log/Append to DOM error message
+  */
   function getRandomGiphy(tagSearch) {
     $.get("http://api.giphy.com/v1/gifs/random",
       {api_key: "dc6zaTOxFJmzC",
       tag: tagSearch}).done(function(response) {
-      var randomPic = response;
-
       $("#display").empty();  //  Clear display
       $("#display").append("<img src=" + response.data.image_url + ">"); //  Append pic to display
-
-
       }).fail(function(response) {
         console.log("fail");
     });
   } //  getRandomGiphy
 
+  /*  Normal Giphy Search function
+  **  Empty the display, append all pictures received from normal search API
+  **  Console.log/Append to DOM error message
+  */
   function getSearchGiphy(normalSearch) {
     if (normalSearch === "") {
       alert("Please input a search or click \"Random Giph\" to randomly generate one. Thank you :D");
@@ -39,6 +43,8 @@ $(function() {
       q: normalSearch,
       api_key: "dc6zaTOxFJmzC"}).done(function(response) {
         $("#display").empty();
+        // console.log(response);
+
         for (var it = 0; it < response.data.length; it++) {
           $("#display").append("<img src=\"" + response.data[it].images.fixed_height.url + "\" />");
         } //  for loop
