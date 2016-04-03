8b4aec4090a4a0e4570fd13c571dbd79041182a1
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-03 08:15:23 -0500

    Fixes Random/Tag & Normal Search Functionality

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 $(function() {
-  //  Event listener for clicking on generate button
-  console.log("Everything works");
+  console.log("Everything works!");
 
-  $(".generator").on("click", function() {
-    //  Clear display
-    $("#display").empty();
-    //  Create variables for tags/searches
+  $("#randomButton").on("click", function() {
     var giphyTag = "";
-    var searchTag = "";
+    /*  Assign tag input value to giphyTag, replacing any spaces if necessary (apparently some giphy tags have spaces)*/
+    giphyTag = $("#tagInput").val().replace(/ /g, "+");
+    console.log(giphyTag);
 
-    giphyTag = $("#giphyTag").val();
-    searchTag = $("#RegSearch").val();
+    getRandomGiphy(giphyTag);
+  }); //  Random/Tag Giphy Button
 
-    //  Get giphy
+  $("#searchButton").on("click", function() {
+    var giphySearch = "";
+    giphySearch = $("#regInput").val().replace(/ /g, "+");
+    console.log(giphySearch);
+
+    getSearchGiphy(giphySearch.toString());
+  }); //  Normal Search Button
+
+  function getRandomGiphy(tagSearch) {
     $.get("http://api.giphy.com/v1/gifs/random",
       {api_key: "dc6zaTOxFJmzC",
-      tag: giphyTag}).done(function(response) {
+      tag: tagSearch}).done(function(response) {
       var randomPic = response;
+      // console.log(randomPic);
+      appendRandomPic(randomPic);
+      }).fail(function(response) {
+        console.log("fail");
+    });
+  } //  getRandomGiphy
 
-      appendRandomPicture(randomPic);
-    }); //  AJAX done
-  }); //  Random and Tag Search
+  function getSearchGiphy(normalSearch) {
+    $.get("http://api.giphy.com/v1/gifs/search", {
+      q: normalSearch,
+      api_key: "dc6zaTOxFJmzC"}).done(function(response) {
+        $("#display").empty();
+        console.log(response.data.length);
+        for (var it = 0; it < response.data.length; it++) {
+          $("#display").append("<img src=\"" + response.data[it].images.fixed_height.url + "\" />");
+        } //  for loop
+      }).fail(function(response){
+        console.log("search fail");
+    });
+  } //  getSearchGiphy
 
-  function appendRandomPicture(picture) {
-    console.log(picture);
-    $("#display").append("<img src=" + picture.data.image_url + ">");
-  } //  appendPicture
-
-}); //  Doc-ready
+  function appendRandomPic(picture) {
+    $("#display").empty();  //  Clear display
+    $("#display").append("<img src=" + picture.data.image_url + ">"); //  Append pic to display
+  } //  appendRandomPic
+}); //  Doc-ready function
