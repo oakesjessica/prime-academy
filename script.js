413c100f1b3bb91b7773de88fbec60a9ad4a736a
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-03 20:47:17 -0500

    Adds comments, calls shuffle function

--- script.js
+++ script.js
@@ -4,62 +4,58 @@ var peerArray = ["Lisa", "Max","Kyle", "Suzanna","Chris. R", "Jessica", "Jennife
 var groupArray =["Tab Index", "Focus", "Title", "On Click", "Access Key", "ContentEditable", "Draggable", "contextMenu", "Dropzone", "SpellCheck"];
 
 function shuffle(array){
- var currentIndex = array.length;
- var randomIndex = 0;
- var tempIndex;
- while (currentIndex !== 0){
-   randomIndex = Math.floor(Math.random() * currentIndex);
-   currentIndex -= 1;
-   tempIndex = array[currentIndex];
-   array[currentIndex] = array[randomIndex];
-   array[randomIndex] = tempIndex;
- }
-return array;
-
+   var currentIndex = array.length;
+   var randomIndex = 0;
+   var tempIndex;
+   while (currentIndex !== 0){
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      tempIndex = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = tempIndex;
+   }
+   return array;
 }
-
 
 var groupNum = 0;
 $(function(){
- $("button").on("click", function() {
+   $("button").on("click", function() {
 
    var buttonId = $(this).parent().attr("id");  // Grab parent ID
 
    if (buttonId == "generate") {
+      $("#display").empty(); // Clear display
 
-     $("#display").empty(); // Clear display
+      // If number is not selected, alert user
+      if (groupNum === 0) {
+         alert("Pick a Number!");
+      }
+      else {
+         var currentGroup;
+         // Append # of group headers to groupNum, taking names from groupArray
+         for(var it = 0; it<groupNum; it++){
+            $("#display").append("<div id=\"G" + (it + 1) +"\" class=\"groups\"><h1>" + " " + groupArray[it] + "</h1></div>");
+         }  // for loop, group names
 
-     // If number is not selected, alert user
-     if (groupNum === 0) {
-       alert("Pick a Number!");
-     }
-     else {
-       var currentGroup;
+         var shuffled = shuffle(peerArray);
+         // Appending shuffled array names to groups
+         for (var gr = 0; gr < shuffled.length; gr++) {
+            currentGroup = gr % groupNum;
 
-       // Append # of group headers to groupNum, taking names from groupArray
-       for(var it = 0; it<groupNum; it++){
-         $("#display").append("<div id=\"G" + (it + 1) +"\" class=\"groups\"><h1>" + " " + groupArray[it] + "</h1></div>");
-       }
+            var groupSelector = "#G" + (currentGroup+1);
 
-       // Appending shuffled array names to groups
-       for (var gr = 0; gr < peerArray.length; gr++) {
-         currentGroup = gr % groupNum;
+            $(groupSelector).append("<p>" + shuffled[gr] + "</p>");
 
-        var groupSelector = "#G" + (currentGroup+1);
+            //  console.log($(groupSelector + " p").last());
 
-         $(groupSelector).append("<p>" + peerArray[gr] + "</p>");
+            $('#display').hide().fadeIn();
+            $(groupSelector + " p").last().hide().delay(gr * 30).fadeIn();
 
-        //  console.log($(groupSelector + " p").last());
-
-         $('#display').hide().fadeIn();
-         $(groupSelector + " p").last().hide().delay(gr * 30).fadeIn();
-
-        //  $(groupSelector + " p").last().show().delay(gr * 60);
-
-       }
-     }
-   }
-   else {
+            //  $(groupSelector + " p").last().show().delay(gr * 60);
+         }  // for loop, shuffled array
+      }  // else statement
+   }  // if statement, generate button
+   else {   // Group # button was clicked
      // Clear display
      $("#display").empty();
 
