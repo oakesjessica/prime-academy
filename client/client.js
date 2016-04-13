e2763f9c929a5a2d6ae6cc0244b4f979f7ef120e
Courtney Ives <cgives@me.com>
2016-04-13 11:38:30 -0500

    add skeleton and base functionality

---
+++ client/client.js
@@ -0,0 +1,57 @@
+var app = angular.module('app', []);
+
+app.controller("MainController", ['$scope', '$http', function($scope, $http){
+  $scope.nouns = [];
+  $scope.adj = [];
+  $scope.randomNames = [];
+  $scope.randNouns = [];
+  $scope.randAdjs = [];
+
+  $scope.getNouns = function(){
+    $http.get('/noun').then(function(response){
+      $scope.nouns = response.data;
+    });
+  };  // $scope.getNouns
+
+  $scope.getAdj = function(){
+    $http.get('/adj').then(function(response){
+      $scope.adj = response.data;
+    });
+  };  // $scope.getAdj
+
+  $scope.shuffle = function(array) {
+
+    var currentIndex = array.length;
+    var tempValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };  //  $scope.shuffle
+
+  $scope.generate = function() {
+    $scope.randNouns = $scope.shuffle($scope.nouns);
+    $scope.randAdjs = $scope.shuffle($scope.adj);
+
+    for (var it = 0; it < $scope.randAdjs.length; it++) {
+      $scope.randomNames.unshift($scope.randNouns[it] + $scope.randAdjs[it]);
+    }
+  };
+
+  // Get nouns/adjectives, generate shuffled result
+  $scope.getNouns();
+  $scope.getAdj();
+  $scope.generate();
+
+
+}]);
