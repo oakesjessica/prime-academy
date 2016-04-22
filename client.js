9681bca4ffd6c498f19cd4a26d4892a2dd99c0b4
Jessica Oakes <jessica.yoonah@gmail.com>
2016-04-22 14:30:39 -0500

    Adds some more styling

--- client.js
+++ client.js
@@ -11,27 +11,24 @@ app.controller("AngularApp", function($http) {
   console.log(vm.currentMessage);
 
   vm.submitMessage = function() {
-    //  If vm.currentMessage is an empty array, first click
+    //  If message input is empty
     if (vm.message === "") {
       //  Display "please enter a message" text
       vm.empty = true;
     } else {
-
       //  Hide "please enter a message" text
       vm.empty = false;
-
       //  If currentMessage array is empty, then this is the first message
       if (vm.currentMessage.length === 0) {
         vm.currentMessage.push(vm.message);
         vm.message = "";
-        console.log(vm.currentMessage);
       } else {
         //  Show Previous Messages div
         vm.previous = true;
-        //  shift old current message to previousMessage array
+        //  shift currentMessage to previousMessage array
         vm.previousMessages.push(vm.currentMessage.shift());
 
-        //  push message to current message
+        //  push message to currentMessage
         vm.currentMessage.push(vm.message);
 
         //  clear input field
